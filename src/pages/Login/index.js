55ff54f9bbc87f24b447d673e45580a14a26795b
Afrizal <afrizalbagas.03@gmail.com>
2020-11-24 23:26:18 +0700

    fix styling and slicing design login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { LoginBg } from "../../assets";
+import { Button, Gap, Input, Link } from "../../components";
 
-const Login = () => {
+const Register = () => {
   return (
-    <div>
-      <p>login page</p>
+    <div className="main-page">
+      <div className="left">
+        <img src={LoginBg} alt="background" className="bg-register" />
+      </div>
+      <div className="right">
+        <p className="title">Login</p>
+        <Input label="Email" placeholder="masukan email anda yang aktif" />
+        <Gap height={20} />
+        <Input label="Password" placeholder="masukan password" />
+        <Gap height={50} />
+        <Button label="Login" />
+        <Link label="Belum Punya Akun ? Register Now" />
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
